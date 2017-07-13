a6c09812de8fead89f6689d3fdf03e0458c04fca
Cecilia <cecilia.torres@iiiepe.edu.mx>
2017-07-12 19:02:42 -0500

    Versión final

--- game.js
+++ game.js
@@ -1,76 +1,63 @@
-// Guardamos las imagenes dos veces en un array y despues cambios su orden aleatoriamente
-var images = [
-  'img/cat00.jpg',
-  'img/cat00.jpg',
-  'img/cat01.jpg',
-  'img/cat01.jpg',
-  'img/cat02.jpg',
-  'img/cat02.jpg',
-  'img/cat03.jpg',
-  'img/cat03.jpg',
-  'img/cat04.jpg',
-  'img/cat04.jpg',
-  'img/cat05.jpg',
-  'img/cat05.jpg',
-  'img/cat06.jpg',
-  'img/cat06.jpg',
-  'img/cat07.jpg',
-  'img/cat07.jpg'
-];
-randomizeImages();
-
-// get images, place them in an array & randomize the order
-/*for (var i = 0; i < 8; i++) { 
+// Creamos un arreglo para guarda las imagenes (2 veces c/u) y despues cambios su orden aleatoriamente
+var images = [];
+for (var i = 0; i < 8; i++) { 
   var img = 'img/cat0' + i + '.jpg';
   images.push(img);
   images.push(img);
-}*/
-
-// Ordenaremos las imagenes en una tabla y ocultaremos su visibilidad (boca abajo)
-var output = "<ol>"; 
-for (var i = 0; i < 16; i++) { 
-  output += "<li>";
-  output += "<img style='display:none' src = '" + images[i] + "'/>";
-  output += "</li>";
 }
-output += "</ol>";
-document.getElementById("container").innerHTML = output;
+randomizeImages();
 
-var guess1 = "";
-var guess2 = "";
-var count = 0;
+// Ordenaremos las imagenes en una lista y ocultaremos su visibilidad (boca abajo)
+var lista = "<ol>"; 
+for (var i = 0; i < 16; i++) { 
+  lista += "<li>";
+  lista += "<img style='display:none' src = '" + images[i] + "'/>";
+  lista += "</li>";
+}
+lista += "</ol>";
+document.getElementById("container").innerHTML = lista;
 
+//
 NodeList.prototype.addEventListener = function(event, func) {
     this.forEach(function(content, item) {
        content.addEventListener(event, func);
     });
 }
 
+// Agregamos un escuchador de eventos a cada tarjeta
+var elements = document.querySelectorAll("li");
+elements.addEventListener("click", voltearCarta);
+
+var guess1 = "";
+var guess2 = "";
+var count = 0;
+
 function voltearCarta() {
-  if(count < 2 && this.children[0].className != 'match') {
+  if(count < 2 && this.firstChild.className != 'match') {
     count ++;
-    this.children[0].style.display = "block";
-    this.children[0].className = "face-up";
+    this.firstChild.style.display = "block";
+    this.firstChild.className = "face-up";
 
     // Intento 1
     if(count === 1) {
-      guess1 = this.children[0].getAttribute("src");
+      guess1 = this.firstChild.getAttribute("src");
     }
     // Intento 2
     else {
-      guess2 = this.children[0].getAttribute("src");
+      guess2 = this.firstChild.getAttribute("src");
       // Apartir del segundo intento, comenzamos a revisar si son iguales
 
       // Acierto!
       if(guess1 == guess2) {
         console.log("Match :)");
-        var correct = document.querySelectorAll('img[src="' + this.children[0].getAttribute("src") + '"]');
+        var correct = document.querySelectorAll('img[src="' + this.firstChild.getAttribute("src") + '"]');
         correct[0].className = "match";
         correct[1].className = "match";
 
       }
       // No acertó :(
       else {
+        console.log("No Match :(");
         // Buscamos todos las imagenes que no hayan acertado
         var incorrect = document.querySelectorAll('img:not(.match)');
         // Y las ocultamos
@@ -87,10 +74,7 @@ function voltearCarta() {
   }
 }
 
-var elements = document.querySelectorAll("li");
-elements.addEventListener("click", voltearCarta);
-
-// randomize array of images
+// Reordenar aleatoriamente las imagenes
 function randomizeImages(){
   Array.prototype.randomize = function() {
     var i = this.length, j, temp;
@@ -101,7 +85,6 @@ function randomizeImages(){
       this[i] = this[j];
       this[j] = temp;
     }
-  };
-  
+  };  
   images.randomize();
 }
